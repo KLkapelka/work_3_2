2fbd8380134df9183badbc3c09cc52d5e7ac4e23
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 10:56:36 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,32 @@
+﻿// Дан файл(nums.txt), содержащий целые числа, через пробел.
+// Удалите из него все четные числа;
+
+using System;
+using System.IO;
+
+class work_3_2
+{
+    static void Main()
+    {
+        string filePath = "nums.txt";
+        string numbers = "11 12 13 14 15 16 17 18 19 20";
+        File.WriteAllText(filePath, numbers);
+        Console.WriteLine("Файл " + filePath + " был создан и заполнен целыми числами.");
+        string data = File.ReadAllText(filePath);
+        string[] numbersStr = data.Split(' ');
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (string number in numbersStr)
+            {
+                int num = int.Parse(number);
+                if (num % 2 != 0)
+                {
+                    writer.Write($"{num} ");
+                }
+            }
+        }
+        Console.WriteLine("Четные числа были удалены из файла.");
+    }
+}
+
+
